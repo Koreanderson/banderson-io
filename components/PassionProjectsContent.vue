d0c58c5c7c69c63332a7b733fc614e63124b1b3a
Brian Anderson <brian.anderson@bvacommerce.com>
2022-05-21 16:23:43 -0400

    Update passion projects content to separate component

---
+++ components/PassionProjectsContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="my-10 md:max-w-lg" v-for="project in projects" :key="project">
+      <h3>{{ project.title }}</h3>
+      <div>{{ project.description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PassionProjectsContent',
+  props: {
+    'projects': Array,
+  },
+  setup() {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/passion-projects.vue
+++ pages/passion-projects.vue
@@ -2,31 +2,28 @@
   <div class="relative py-20 mx-auto mb-20 max-w-screen-xl before:-left-24 before:bg-primary before:block before:top-0 before:bottom-0 before:w-52 before:absolute">
     <div class="relative max-w-xl">
       <h1>{{ page.title }}</h1>
-      <div class="my-10 md:max-w-lg" v-for="project in page.projects" :key="project">
-        <h3>{{ project.title }}</h3>
-        <div>
-          {{ project.description }}
-        </div>
-      </div>
+      <PassionProjectsContent :projects="page.projects" />
     </div>
   </div>
 </template>
 
 <script>
+import PassionProjectsContent from '../components/PassionProjectsContent.vue';
 export default {
-  name: 'Passion Projects',
-  transitions: 'page',
-  async asyncData({ $content, params, error }) {
-    const page = await $content('passion-projects')
-      .fetch()
-      .catch(err => {
-        error({ statusCode: 404, message: "Page not found" });
-      });
-    return {
-      page
-    };
-  },
-  setup() {},
+    name: "PassionProjects",
+    transitions: "page",
+    async asyncData({ $content, params, error }) {
+        const page = await $content("passion-projects")
+            .fetch()
+            .catch(err => {
+            error({ statusCode: 404, message: "Page not found" });
+        });
+        return {
+            page
+        };
+    },
+    setup() { },
+    components: { PassionProjectsContent }
 }
 </script>
 

a8fea0d9fa12ebffc6b69d945f8462c50332a2fc
Brian Anderson <brian.anderson@bvacommerce.com>
2022-05-26 10:04:30 -0400

    Updates

---
+++ _nuxt/static/1653573854/passion-projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbanderson-io\u002F_nuxt\u002Fstatic\u002F1653573854",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpassion-projects",config:{_app:{basePath:"\u002Fbanderson-io\u002F",assetsPath:"\u002Fbanderson-io\u002F_nuxt\u002F",cdnURL:a}}}}(null));

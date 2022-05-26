a33966142185dd008dc251ca731b96fa31bda119
Brian Anderson <brian.anderson@bvacommerce.com>
2022-05-26 10:09:37 -0400

    Updates

---
+++ _nuxt/static/1653574173/experience/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbanderson-io\u002F_nuxt\u002Fstatic\u002F1653574173",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexperience",config:{_app:{basePath:"\u002Fbanderson-io\u002F",assetsPath:"\u002Fbanderson-io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
